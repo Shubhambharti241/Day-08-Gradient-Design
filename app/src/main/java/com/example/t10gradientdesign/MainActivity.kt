339ab359f916cde4f0ef5a5d6b50a2d469ec676e
Shubhambharti241 <shubhambharti1290@gmail.com>
2024-08-08 13:05:54 +0530

    Gradient Design

---
+++ app/src/main/java/com/example/t10gradientdesign/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.t10gradientdesign
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        Handler().postDelayed({
+
+            val intent = Intent(this ,SignUp::class.java )
+            startActivity(intent)
+            finish()
+        },3000)
+
+    }
+}
